4942c4cff0b48020ec8598e4063730910a7715ad
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-18 16:03:36 +0700

    a

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -101,12 +101,8 @@ class CropAndExtract():
             crop_info = ((ox2 - ox1, oy2 - oy1), crop, quad)
             print("day la crop")
         elif 'full' in crop_or_resize.lower():
-            x_full_frames, crop, quad = self.propress.crop_(x_full_frames, still=True if 'ext' in crop_or_resize.lower() else False, xsize=512)
-            clx, cly, crx, cry = crop
-            lx, ly, rx, ry = quad
-            lx, ly, rx, ry = int(lx), int(ly), int(rx), int(ry)
-            oy1, oy2, ox1, ox2 = cly+ly, cly+ry, clx+lx, clx+rx
-            crop_info = ((ox2 - ox1, oy2 - oy1), crop, quad)
+            oy1, oy2, ox1, ox2 = 0, x_full_frames[0].shape[0], 0, x_full_frames[0].shape[1] 
+            crop_info = ((ox2 - ox1, oy2 - oy1), None, None)
             print("day la full")
 
         else: # resize mode
