666a160728d2f098f51226734831721e184f83cc
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-18 14:20:35 +0700

    a

--- src/utils/croper.py
+++ src/utils/croper.py
@@ -140,7 +140,7 @@ class Preprocesser:
             _inp = img_np_list[_i]
             _inp = cv2.resize(_inp, (rsize[0], rsize[1]))
             _inp = _inp[cly:cry, clx:crx]
-            red = [0,0,255]
+            red = [0,255,0]
             _inp[10,5] = red
 
             cv2.imwrite("result.png",_inp)
