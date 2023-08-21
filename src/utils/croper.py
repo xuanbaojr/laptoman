9370aaabc4ab3127a2c40344d8f8dfe96600427a
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-21 09:56:34 +0700

    a

--- src/utils/croper.py
+++ src/utils/croper.py
@@ -149,7 +149,7 @@ class Preprocesser:
         for _i in range(len(img_np_list)):
             _inp = img_np_list[_i]
             _inp = cv2.resize(_inp, (rsize[0], rsize[1]))
-         #   _inp = _inp[cly:cry, clx:crx]
+            _inp = _inp[cly:cry, clx:crx]
             print(clx, cly, crx, cry, rsize[0], rsize[1], lx, ly, rx, ry)
 
 
