ec75d27fcebfa4e075e55f3dc243560162bb32cc
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-21 13:22:50 +0700

    a

--- src/utils/croper.py
+++ src/utils/croper.py
@@ -154,7 +154,7 @@ class Preprocesser:
             crx = min(int((crx + (crx - clx)/2)),rsize[0])
             clx = max(int((clx - (crx - clx)/2)),0)
             cly = max(int((cly - (cry - cly)/2)),0)
-            crx = max(int((cry + (cry - cly)/2)),rsize[1])
+            cry = max(int((cry + (cry - cly)/2)),rsize[1])
                 
             _inp = _inp[cly:cry  , clx:crx]
 
