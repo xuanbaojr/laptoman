1e700c4475b71dad988952832fa2bd5ddb8f39f1
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-21 09:39:51 +0700

    a

--- src/utils/croper.py
+++ src/utils/croper.py
@@ -125,7 +125,7 @@ class Preprocesser:
     
     def crop(self, img_np_list, still=False, xsize=512):    # first frame for all video
 
-        rate = 0.3
+        rate = 0.9
     
         img_np = img_np_list[0]
         lm = self.get_landmark(img_np)
