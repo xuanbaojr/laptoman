db32b4ec49837b273c34a672c422f0b333749901
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-22 11:13:34 +0700

    a

--- src/pre_image.py
+++ src/pre_image.py
@@ -1,6 +1,6 @@
 from src.utils.croper import Preprocesser
 
-import cv2, torch
+import cv2, torch, os
 import numpy as np
 from PIL import Image
 
@@ -9,12 +9,32 @@ class Image_Preprocess():
         self.divice = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.preprocess = Preprocesser()
 
-    def img_pre(self, source_image, xsize = 512):
+    def img_pre(self, input_path, xsize = 512):
 
         rate = 5
         rate_ = 2
 
-        img_np = source_image[0]
+        if not os.path.isfile(input_path):
+            raise ValueError('input_path must be a valid path to video/image file')
+        elif input_path.split('.')[-1] in ['jpg', 'png', 'jpeg']:
+            # loader for first frame
+            full_frames = [cv2.imread(input_path)]
+            fps = 25
+        else:
+            # loader for videos
+            video_stream = cv2.VideoCapture(input_path)
+            fps = video_stream.get(cv2.CAP_PROP_FPS)
+            full_frames = [] 
+            while 1:
+                still_reading, frame = video_stream.read()
+                if not still_reading:
+                    video_stream.release()
+                    break 
+                full_frames.append(frame) 
+
+
+        x_full_frames= [cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  for frame in full_frames] 
+        img_np = x_full_frames[0]
         lm = self.preprocess.get_landmark(img_np)
 
         rsize, crop, _ = self.preprocess.align_face(img = Image.fromarray(img_np), lm = lm, output_size=xsize)
