61cc61d29dbd3866fc6bea04c556aa862e06a225
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-23 13:06:00 +0700

    a

--- src/facerender/modules/make_animation.py
+++ src/facerender/modules/make_animation.py
@@ -107,7 +107,6 @@ def make_animation(source_image, source_semantics, target_semantics,
         predictions = []
         # hinh nhu day moi la keypoint, vi source image theo no dang le chi co dau -> thu test source_image = source_image (face)
         kp_canonical = kp_detector(source_image) * 0.1
-        print("source_image_make_animation.py" , source_image)   
         print("kp_canonical_make_animation.py" , kp_canonical)   
         he_source = mapping(source_semantics)
         kp_source = keypoint_transformation(kp_canonical, he_source)
