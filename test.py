42f6dfeb30b0435641452b22942cc6f202ce64ec
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-24 18:36:10 +0700

    a

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def paste_video(video_path_01, video_path_02):
     frame_w = 256
     frame_h = 256
 
-    face_h = int(156*1.1.07)
+    face_h = int(156*1.107)
     face_w = int(114*1.107)
 
     tmp_path = 'test/test.mp4'
