96b91bbb5fb7eb275aa7a9f89b8b0b22aca858a0
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-24 08:07:33 +0700

    a

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -93,12 +93,12 @@ class CropAndExtract():
 
         #### crop images as the 
         if 'crop' in crop_or_resize.lower(): # default crop
-            x_full_frames, crop, quad = self.propress.crop(x_full_frames, still=True if 'ext' in crop_or_resize.lower() else False, xsize=512)
+            x_full_frames, crop, quad = self.propress.crop_(x_full_frames, still=True if 'ext' in crop_or_resize.lower() else False, xsize=512)
             clx, cly, crx, cry = crop
             lx, ly, rx, ry = quad
             lx, ly, rx, ry = int(lx), int(ly), int(rx), int(ry)
             oy1, oy2, ox1, ox2 = cly+ly, cly+ry, clx+lx, clx+rx
-            crop_info = ((rx - lx, ry - ly), crop, quad)
+            crop_info = ((ox2 - ox1, oy2 - oy1), crop, quad)
             print("day la crop")
         elif 'full' in crop_or_resize.lower():
             x_full_frames, crop, quad = self.propress.crop_(x_full_frames, still=True if 'ext' in crop_or_resize.lower() else False, xsize=512)
