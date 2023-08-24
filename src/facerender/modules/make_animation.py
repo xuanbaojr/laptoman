e90978c7b5103fc88b633858b9257ef1c3726ba8
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-24 14:58:43 +0700

    a

--- src/facerender/modules/make_animation.py
+++ src/facerender/modules/make_animation.py
@@ -140,7 +140,7 @@ def make_animation(source_image, source_semantics, target_semantics,
             kp_norm = kp_driving
             for key in kp_driving:
 
-                kp_driving[key][:,1] = 0.5
+                kp_driving[key][:,2] = 0.5
 
 
 
