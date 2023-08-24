e3c518b56941b1aeeed4932621983d7615feb787
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-24 14:23:03 +0700

    a

--- src/facerender/modules/make_animation.py
+++ src/facerender/modules/make_animation.py
@@ -140,19 +140,13 @@ def make_animation(source_image, source_semantics, target_semantics,
                 he_driving['roll_in'] = roll_c_seq[:, frame_idx] 
             
             kp_driving = keypoint_transformation(kp_canonical, he_driving)
-        #    for key in kp_driving:
-         #       kp_driving[key][:,0] *= 0
-          #      kp_driving[key][:,1] *= 1
-           #     kp_driving[key][:,2] *= 2
+
                 
             kp_norm = kp_driving
             for key in kp_driving:
 
-                kp_driving[key][:,0] *= 0.75
-                kp_driving[key][:,1] *= 0.75
-                kp_driving[key][:,2] *= 0.75
-                kp_driving[key][:,3] *= 0.75
-                kp_driving[key][:,4] *= 0.75
+           #     kp_driving[key][:,0] *= 0.75
+
 
             out = generator(source_image, kp_source=kp_source, kp_driving=kp_driving)
             print("kp_driving" , kp_driving)
