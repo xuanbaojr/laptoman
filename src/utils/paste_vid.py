10acbc6f14919f9ea37b915227cf6c14e573925c
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-28 10:03:59 +0700

    a

--- src/utils/paste_vid.py
+++ src/utils/paste_vid.py
@@ -38,21 +38,26 @@ def paste_vid(head_video, body_video, crop_info, new_audio_path, full_video_path
     frame_w = 256
     frame_h = 256
 
-    head_w = int((crx - clx)*(256/ body_w))
-    head_h = int((cry - cly)*(256/ body_h))
-    print("head_w, head_h", head_w, head_h)
 
-    clx = int(clx*(256/body_w))
-    crx = int(crx*(256/body_w))
-    cly = int(cly*(256/body_h))
-    cry = int(cry*(256/body_h))
+        
+    clx, cly, crx, cry = crop_info[1]
+    lx, ly, rx, ry = crop_info[2]
+    lx, ly, rx, ry = int(lx), int(ly), int(rx), int(ry)
+    oy1, oy2, ox1, ox2 = cly+ly, cly+ry, clx+lx, clx+rx
+
+    ox1 = int(ox1*(256/body_w))
+    ox2 = int(ox2*(256/body_w))
+    oy1 = int(oy1*(256/body_h))
+    oy2 = int(oy2*(256/body_h))
+
+
 
     tmp_path = str(uuid.uuid4())+'.mp4'
     out_tmp = cv2.VideoWriter(tmp_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (frame_w, frame_h))
     for key in tqdm(range(len(full_frame_head)), 'Dang noi video'):
-        head = cv2.resize(full_frame_head[key].astype(np.uint8), (head_w, head_h))
+        head = cv2.resize(full_frame_head[key].astype(np.uint8), (ox2 - ox1, oy2 - oy1))
         mask = 255*np.ones(head.shape, head.dtype)
-        location = ((clx+crx)//2 ,( cly + cry ) //2 )
+        location = ((ox1+ox2)//2 ,( oy1 + oy2 ) //2 )
         gen_img = cv2.seamlessClone(head, full_frame_body[key], mask, location, cv2.NORMAL_CLONE)
         out_tmp.write(gen_img)
 
