08725d0a173a3c12ebcb57376d4fa2d4f65c6358
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-28 15:10:10 +0700

    a

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import os
 from src.gradio_demo import SadTalker
-# warpfn , lazy_load ?
 def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', config_path='src/config', warpfn=None):
     sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
-    # source image :
-
 
     if os.path.exists(source_image):
         print("Source_image is exits!")
     else:
         print("Source_image is not exits!")
 
-    preprocess = "full"
-    print(preprocess)
-    
+    preprocess = "full"    
     result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True)
-
     return result
     
 
 if __name__ == "__main__":
-    source_image = 'test/full3.png'
+    source_image = 'test/art_0.jpg'
     driven_audio = 'test/test_vi.wav'
     demo = sadtalker_demo(source_image, driven_audio)
     print(demo)
