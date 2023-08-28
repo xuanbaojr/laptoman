3b1e1951abeb41420a73c9562f495baf6a129f68
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-28 09:04:46 +0700

    a

--- src/utils/paste_vid.py
+++ src/utils/paste_vid.py
@@ -41,12 +41,15 @@ def paste_vid(head_video, body_video, crop_info, new_audio_path, full_video_path
     head_w = int(256*((crx - clx)/ body_w))
     head_h = int(256*((cry - cly)/ body_h))
 
+    location_w = int(256*((clx+crx)//2)/ body_w)
+    location_h = int(256*((cly+cry)//2)/ body_h)
+
     tmp_path = str(uuid.uuid4())+'.mp4'
     out_tmp = cv2.VideoWriter(tmp_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (frame_w, frame_h))
     for key in tqdm(range(len(full_frame_head)), 'Dang noi video'):
         head = cv2.resize(full_frame_head[key].astype(np.uint8), (head_w, head_h))
         mask = 255*np.ones(head.shape, head.dtype)
-        location = (100,100 )
+        location = (location_w, location_h )
         gen_img = cv2.seamlessClone(head, full_frame_body[key], mask, location, cv2.NORMAL_CLONE)
         out_tmp.write(gen_img)
 
