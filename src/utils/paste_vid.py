146296c077e99160e90a2168c2d0bc2758f6e26d
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-28 08:01:49 +0700

    a

--- src/utils/paste_vid.py
+++ src/utils/paste_vid.py
@@ -5,12 +5,32 @@ import uuid
 
 from src.utils.videoio import save_video_with_watermark 
 
-def paste_vid(head_video, body_video, crop_info, new_audio_path, full_video_path):
+def paste_vid(head_video, body_video, crop_info, new_audio_path, full_video_path, pic_path):
 
     print("head_video", head_video)
     print("body_video", body_video)
     if not os.path.isfile(head_video):
         print('ko thay file')
+
+    if not os.path.isfile(pic_path):
+        raise ValueError('pic_path must be a valid path to video/image file')
+    elif pic_path.split('.')[-1] in ['jpg', 'png', 'jpeg']:
+        # loader for first frame
+        full_img = cv2.imread(pic_path)
+    else:
+        # loader for videos
+        video_stream = cv2.VideoCapture(pic_path)
+        fps = video_stream.get(cv2.CAP_PROP_FPS)
+        full_frames = [] 
+        while 1:
+            still_reading, frame = video_stream.read()
+            if not still_reading:
+                video_stream.release()
+                break 
+            break 
+        full_img = frame
+    frame_h_head = full_img.shape[0]
+    frame_w_head = full_img.shape[1]
 
     video_head = cv2.VideoCapture(head_video)
     fps = 25
@@ -43,7 +63,7 @@ def paste_vid(head_video, body_video, crop_info, new_audio_path, full_video_path
     for key in tqdm(range(len(full_frame_head)), 'Dang noi video'):
         head = cv2.resize(full_frame_head[key].astype(np.uint8), (10, 10))
         mask = 255*np.ones(head.shape, head.dtype)
-        location = ((clx + crx)//2 , (cly + cry)//2 )
+        location = (20,20 )
         gen_img = cv2.seamlessClone(head, full_frame_body[key], mask, location, cv2.NORMAL_CLONE)
         out_tmp.write(gen_img)
 
