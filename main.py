a806af48dcf7db032a06c513b396762119a0c4be
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-29 07:38:32 +0700

    a

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', co
     
 
 if __name__ == "__main__":
-    source_image = 'test/art_3.png'
+    source_image = 'test/art_5.jpg'
     driven_audio = 'test/test_vi.wav'
     demo = sadtalker_demo(source_image, driven_audio)
     print(demo)
 
+# sinh -> chu dong sinh ko background
