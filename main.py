7d033e5b02dae1291c2a7b30cea3d2b65958736f
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-08-29 10:16:32 +0700

    a

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', co
         print("Source_image is not exits!")
 
     preprocess = "full"    
-    result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = True)
+    result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = False)
     return result
     
 
