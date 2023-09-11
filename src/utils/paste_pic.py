95d4091dff068e216d55ce38e9eb11f433f8035a
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-11 17:26:25 +0700

    a

--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -10,7 +10,7 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
 
 
     full_img = cv2.imread(pic_path)
-    full_img = cv2.resize(full_img, (512,512))
+    full_img = cv2.resize(full_img, (256,256))
     full_img = full_img.astype(np.uint8)
     frame_w = full_img.shape[0]
     frame_h = full_img.shape[1]
