7bac21bd7e24c4edfd49596186d9e7b2d4aed7bc
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-11 13:03:43 +0700

    a

--- launcher.py
+++ launcher.py
@@ -3,40 +3,25 @@ import numpy as np
 
 # Read grayscale image
 image = cv2.imread('test/test.png')
-h, w = image.shape[:2]
+image_ = cv2.imread('test/test.png')
 
-for i in range(h):
-    for j in range(w):
-        if (image[i,j] == np.array([0,0,0])).all():
-            image[i,j] = [255,255,0]
-#cv2.imshow('Original1', image)
+# Changing black pixels to [110,255,0]
+image[np.all(image == [0,0,0], axis=2)] = [255,255,255]
 cv2.imwrite('test/result_img01.png', image)
-
-
 
 image_test = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply adaptive thresholding
-adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
 
-# Loop to manipulate image
-for i in range(h):
-    for j in range(w - 1):  # Change here to avoid index out of bound
-        if adaptive_threshold_image[i, j] == 0 and adaptive_threshold_image[i, j + 1] == 255:  # Change here to correct the indices
-            image[i, 0:j, :] = [0,0,0]
-            break
-    for j in range(w-1, 0, -1):
-        if adaptive_threshold_image[i, j] == 0 and adaptive_threshold_image[i, j - 1] == 255:  # Change here to correct the indices
-            image[i, j:w-1, :] = [0,0,0]
-            break
+adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 65)
 
-# Show images
-for i in range(h):
-    for j in range(w):
-        if (image[i,j] == np.array([0,0,0])).all():
-            image[i,j] = [255,255,0]
+# This will set pixels to [110, 255, 0] where adaptive_threshold_image is 0, 
+# otherwise it will keep the original values from `image`
+image = np.where(adaptive_threshold_image[:, :, None] == 0, [255, 255, 255], image)
+image_[np.where(adaptive_threshold_image == 0)] = [255,255,255]
+#image = np.where(image[:, :, :] == [110,255,0], backgound, image)
+
+cv2.imwrite('test/result_img02.png', image_)
+
 cv2.imwrite('test/result_img.png', image)
 cv2.imwrite('test/threshold.png',adaptive_threshold_image)
-
-
-
--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -2,51 +2,28 @@ import cv2, os
 import numpy as np
 from tqdm import tqdm
 import uuid
+from rembg import remove
 
 from src.utils.videoio import save_video_with_watermark 
 
 def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, extended_crop=False):
 
-    print("video_path", video_path)
-    
-    print("pic_path", pic_path)
-   # pic_path = "./results/art_0_360.png"
-    print("pic_path", pic_path)
-    if not os.path.isfile(pic_path):
-        print('ko thay file')
 
-
-
-    if not os.path.isfile(pic_path):
-        raise ValueError('pic_path must be a valid path to video/image file')
-    elif pic_path.split('.')[-1] in ['jpg', 'png', 'jpeg']:
-        # loader for first frame
-        full_img = cv2.imread(pic_path)
-        print("full_img", full_img)
-    else:
-        # loader for videos
-        video_stream = cv2.VideoCapture(pic_path)
-        fps = video_stream.get(cv2.CAP_PROP_FPS)
-        full_frames = [] 
-        while 1:
-            still_reading, frame = video_stream.read()
-            if not still_reading:
-                video_stream.release()
-                break 
-            break 
-        full_img = frame
+    full_img = cv2.imread(pic_path)
     full_img = cv2.resize(full_img, (512,512))
-    frame_h = full_img.shape[0]
-    frame_w = full_img.shape[1]
-    print("full_img", full_img.shape)
+    frame_w = full_img.shape[0]
+    frame_h = full_img.shape[1]
+
+    # output_path = 'test/test4.png'
+
+    # with open(pic_path, 'rb') as i:
+    #     with open(output_path, 'wb') as o:
+    #         input = i.read()
+    #         output = remove(input)
+    #         o.write(output)
+    # crop_img = cv2.imread(output_path)
 
 
-
-
-    value = 0  # Giá trị này có thể thay đổi tùy ý, tùy vào mức độ giảm sáng bạn muốn
-    full_img = cv2.subtract(full_img, np.ones(full_img.shape, dtype="uint8") * value)
-
-    blur_img = cv2.blur(full_img, (5,5))
 
 
     video_stream = cv2.VideoCapture(video_path)
@@ -81,27 +58,18 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
     out_tmp = cv2.VideoWriter(tmp_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (frame_w, frame_h))
     for crop_frame in tqdm(crop_frames, 'seamlessClone:'):
 
-        test_img = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
-        test_img = cv2.adaptiveThreshold(test_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 2)
-        value_ = 0
-        crop_frame = cv2.subtract(crop_frame, np.ones(crop_frame.shape, dtype="uint8") * value_)
-
-        p = (crop_frame.astype(np.uint8))
-
-
-        mask = 255*np.ones(p.shape, p.dtype)
-        location = ((p.shape[0]) // 2, (p.shape[1]) // 2)
-        gen_img = cv2.seamlessClone(p, full_img, mask, location, cv2.NORMAL_CLONE)
-        for i in range (frame_h):
-            for j in range(frame_w - 1):  # Change here to avoid index out of bound
-                if test_img[i, j] == 0 :  # Change here to correct the indices
-                    crop_frame[i, 0:j+11, :] = np.copy(full_img[i,0:j+11, :])
-                    
-                    break
-            for j in range(frame_w-1, 0, -1):
-                if test_img[i, j] == 0 :  # Change here to correct the indices
-                    crop_frame[i, j-11:frame_w-1, :] = np.copy(full_img[i, j-11:frame_w-1, :])
-                    break
+       
+        # p = (crop_frame.astype(np.uint8))
+        # mask = 255*np.ones(p.shape, p.dtype)
+        # location = ((p.shape[0]) // 2, (p.shape[1]) // 2)
+        # gen_img = cv2.seamlessClone(p, full_img, mask, location, cv2.NORMAL_CLONE)
+        crop_frame[np.all(crop_frame == (0,0,0), axis=2)] = [255,255,255]
+        img_blur = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
+        blur_img = cv2.blur(full_img)
+        adaptive_img = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 65)
+        crop_frame = np.where(adaptive_img[:,:,None] == 0, [255,255,255], crop_frame)
+        crop_frame[np.where(crop_frame = [255,255,255])] = np.copy(full_img)
+        crop_frame[np.where(adaptive_img[:,:,None] == 0)] = np.copy(blur_img)
 
       #  crop_frame = cv2.GaussianBlur(crop_frame, (15,15), 0)
         out_tmp.write(crop_frame)
