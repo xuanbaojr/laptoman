2a049ee04029c7f69a2a7a2f254ae91c72f9241d
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-11 14:43:23 +0700

    a

--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -66,10 +66,10 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
         crop_frame[np.all(crop_frame == (0,0,0), axis=2)] = [255,255,255]
         img_blur = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
         blur_img = cv2.blur(full_img, (49,49))
-     #   adaptive_img = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 65)
-     #   crop_frame = np.where(adaptive_img[:,:,None] == 0, [255,255,255], crop_frame)
+        adaptive_img = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 65)
+        crop_frame = np.where(adaptive_img[:,:,None] == 0, [255,255,255], crop_frame)
         crop_frame = np.where(crop_frame[:,:,:] == [255,255,255], full_img, crop_frame)
-     #   crop_frame = np.where(adaptive_img[:,:,None] == 0, blur_img, crop_frame)
+        crop_frame = np.where(adaptive_img[:,:,None] == 0, blur_img, crop_frame)
         crop_frame = crop_frame.astype(np.uint8)
       #  crop_frame = cv2.GaussianBlur(crop_frame, (15,15), 0)
         cv2.imwrite('haha.png',crop_frame[0])
