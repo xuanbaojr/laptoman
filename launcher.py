24850dda50c20b1aaf0b3377f770e8e30a6ca6f5
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-12 14:24:59 +0700

    a

--- launcher.py
+++ launcher.py
@@ -13,7 +13,7 @@ image_test = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply adaptive thresholding
 
-adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 50)
+adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 50)
 
 # This will set pixels to [110, 255, 0] where adaptive_threshold_image is 0, 
 # otherwise it will keep the original values from `image`
@@ -22,7 +22,7 @@ image = np.where(adaptive_threshold_image[:, :, None] == 0, [112,110,254], image
 
 adaptive_threshold_image = adaptive_threshold_image.astype(np.uint8)
 
-kernel = np.ones((2,2), np.uint8)
+kernel = np.ones((5,5), np.uint8)
 adaptive_threshold_image = cv2.dilate(adaptive_threshold_image, kernel, iterations=1)
 
 
