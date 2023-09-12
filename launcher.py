03df39ed153128e921e629f54c47cf77c07294df
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-12 11:17:30 +0700

    a

--- launcher.py
+++ launcher.py
@@ -13,18 +13,18 @@ image_test = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply adaptive thresholding
 
-adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 85)
+adaptive_threshold_image = cv2.adaptiveThreshold(image_test, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 50)
 
 # This will set pixels to [110, 255, 0] where adaptive_threshold_image is 0, 
 # otherwise it will keep the original values from `image`
 image = np.where(adaptive_threshold_image[:, :, None] == 0, [255, 255, 255], image)
 image = np.where(adaptive_threshold_image[:, :, None] == 0, [112,110,254], image)
 
-        
-image_[np.where(adaptive_threshold_image == 0)] = [255,255,255]
-#image = np.where(image[:, :, :] == [110,255,0], backgound, image)
+adaptive_threshold_image = adaptive_threshold_image.astype(np.uint8)
 
-cv2.imwrite('test/result_img02.png', image_)
+kernel = np.ones((2,2), np.uint8)
+adaptive_threshold_image = cv2.dilate(adaptive_threshold_image, kernel, iterations=1)
+
 
 cv2.imwrite('test/result_img.png', image)
 cv2.imwrite('test/threshold.png',adaptive_threshold_image)
