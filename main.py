2181ecb8a1739f5bfe24436a827f07d2f8ab3dad
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-14 15:56:40 +0700

    a

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from api.src.inference import SadTalker
+def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', config_path='src/config', warpfn=None):
+    sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
+
+    if os.path.exists(source_image):
+        print("Source_image is exits!")
+    else:
+        print("Source_image is not exits!")
+
+    preprocess = "full"    
+    result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = False)
+    return result
+    
+
+if __name__ == "__main__":
+
+    source_image = 'test/art_0.jpg'
+    driven_audio = 'test/bus_chinese_out.wav'
+    demo = sadtalker_demo(source_image, driven_audio)
+    print(demo)
+
+# sinh -> chu dong sinh ko background
