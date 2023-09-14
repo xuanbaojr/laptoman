7102f205b35dc5949982df859f9ab2f94a616137
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-14 14:01:43 +0700

    a

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', co
 
 if __name__ == "__main__":
 
-    source_image = 'test/art_5.png'
+    source_image = 'test/art_1.jpg'
     driven_audio = 'test/test_vi.wav'
     demo = sadtalker_demo(source_image, driven_audio)
     print(demo)
