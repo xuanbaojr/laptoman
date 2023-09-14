c96e7c06d2a3a262d725a7c6e23884a1cccc0cc3
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-14 16:10:41 +0700

    a

--- api/main.py
+++ api/main.py
@@ -1,22 +1,25 @@
-import os
+import os, cv2
 from src.inference import SadTalker
 def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', config_path='src/config', warpfn=None):
-    sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
 
-    if os.path.exists(source_image):
-        print("Source_image is exits!")
-    else:
-        print("Source_image is not exits!")
+    img = cv2.imread(source_image)
+    print(img)
+    # sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
 
-    preprocess = "full"    
-    result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = False)
-    return result
+    # if os.path.exists(source_image):
+    #     print("Source_image is exits!")
+    # else:
+    #     print("Source_image is not exits!")
+
+    # preprocess = "full"    
+    # result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = False)
+    # return result
     
 
 if __name__ == "__main__":
 
-    source_image = './s/art_0.jpg'
-    driven_audio = 's/bus_chinese_out.wav'
+    source_image = 'test/art_0.jpg'
+    driven_audio = 'test/bus_chinese_out.wav'
     demo = sadtalker_demo(source_image, driven_audio)
     print(demo)
 
