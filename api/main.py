709dbc71540f5e235536c9fa7e30454c1f6cb2f3
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-14 16:11:13 +0700

    a

--- api/main.py
+++ api/main.py
@@ -2,14 +2,13 @@ import os, cv2
 from src.inference import SadTalker
 def sadtalker_demo(source_image, driven_audio, checkpoint_path='checkpoints', config_path='src/config', warpfn=None):
 
-    img = cv2.imread(source_image)
-    print(img)
+   
     # sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
 
-    # if os.path.exists(source_image):
-    #     print("Source_image is exits!")
-    # else:
-    #     print("Source_image is not exits!")
+    if os.path.exists(source_image):
+        print("Source_image is exits!")
+    else:
+        print("Source_image is not exits!")
 
     # preprocess = "full"    
     # result = sad_talker.test(source_image, driven_audio, preprocess, still_mode = True, use_enhancer = False)
