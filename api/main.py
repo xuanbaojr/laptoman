b058b06f53843dcd2a3979571541d9c61758bb43
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-09-18 11:56:12 +0700

    a

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     source_image = 'test/art_0.jpg'
     driven_audio = 'test/test_vi.wav'
-    preprocess = "full"
+    preprocess = "crop"
     still_mode = False
     demo = sadtalker_demo(source_image, driven_audio, preprocess, still_mode )
     print(demo)
